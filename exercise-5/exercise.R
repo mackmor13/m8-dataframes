bd7aa44f94616dd734846270c8f79cb208433a6f
mackmor13 <mackaay@uw.edu>
2016-10-13 15:07:23 -0700

    Lecture Practice

--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -1,36 +1,40 @@
 # Exercise 5: Gates Foundation Educational Grants
 
-# Read data into a variable called `grants` using the `read.csv` function
+setwd("~/Info_201/m8-dataframes/exercise-5")
 
+# Read data into a variable called `grants` using the `read.csv` function
+grants <- read.csv('data/gates_money.csv', stringsAsFactors = FALSE) 
 
 # Use the View function to look at your data
-
+View(grants)
 
 # Create a variable `spending` as the `total_amount` column of the dataset
-
+spending <- sum(grants$total_amount[1:255])
 
 # Confirm that your `spending` variable is a vector using the `is.vector` function
-
+is.vector(spending)
 
 # Create a variable `org` as the `organization` column of the dataset.
 # Unfortunately, it will not be a vector by default, so you must transform it using the as.vector function
-
+org <- as.vector(grants$organization)
 
 ### Now you can ask some more interesting questions about the dataset.  Store your answers in variables ###
 
-# What was the mean grant value?
 
+# What was the mean grant value?
+mean.spending <- mean(grants$total_amount)
 
 # What was the dollar amount of the largest grant?
-
+highest.amount <- max(spending)
 
 # What was the dollar amount of the smallest grant?
-
+lowest.amount <- min(spending)
 
 # Which organization received the largest grant?
-
+largest.recipient <- org[spending == lowest.amount]
 
 # Which organization received the smallest grant?
-
+smalles.recipient <- org[spending == lowest.amount]
 
 # How many grants were awarded in 2010?
+length(spending[grants$start_year == 2010])
